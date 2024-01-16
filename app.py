6313faefac8d9bed7616bdfd30c665555010d718
Satyendra Prakash <satyendra679017@gmail.com>
2024-01-16 13:02:16 +0530

    Added scikit-learn module

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 import streamlit as st
 import pickle
 import time
+import os
 
-#loading model
+# Get the directory of the current script
+current_directory = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(current_directory, 'sentiment_analysis.pkl')
 
-model = pickle.load(open('sentiment_analysis.pkl', 'rb'))
+# Load model
+try:
+    with open(model_path, 'rb') as model_file:
+        model = pickle.load(model_file)
+except Exception as e:
+    print(f"Error loading the model: {e}")
+    st.error(f"Error loading the model: {e}")
 
-# creating title
-
+# Creating title
 st.title('Sentiment Analysis Model')
 
+# User input
 review = st.text_input('Enter your Review')
 
+# Prediction button
 submit = st.button('Predict')
 
 if submit:
-     
-     start = time.time()
-     prediction = model.predict([review])
+    start = time.time()
+    prediction = model.predict([review])
+    end = time.time()
 
-     end = time.time()
+    st.write('Prediction time taken:', round(end - start, 2))
 
-     st.write('Prediction time taken:' , round(end-start, 2 ))
-
-     if prediction[0]=='positive':
-           st.success('Positive Review')
-
-     else: 
-           st.warning ('Negative Review')
+    if prediction[0] == 'positive':
+        st.success('Positive Review')
+    else:
+        st.warning('Negative Review')
